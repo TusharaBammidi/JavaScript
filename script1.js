c8895ab39a6f12364c800d6dafd30dbdb1f4584d
TusharaBammidi <tusharabammidi@thestackly.com>
2026-07-17 10:54:53 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,140 @@
+// Task-1 Employee Registration//
+const company = "Stackly";
+let employeeId = "EMP205";
+let employeeName = prompt("Enter Employee Name");
+
+console.log("Company :", company);
+console.log("Employee Name :", employeeName);
+console.log("Employee ID :", employeeId);
+
+// Task-2 College Admission //
+const collegeName = "Sunrise Institute of Technology";
+let studentName = prompt("Enter Student Name");
+let course = prompt("Enter Course");
+
+console.log("College :", collegeName);
+console.log("Student :", studentName);
+console.log("Course :", course);
+
+
+// Task-3 Food Order //
+const restaurantName = "Spice Villa";
+let customerName = prompt("Enter Customer Name");
+let foodName = prompt("Enter Food Name");
+let quantity = 3;
+
+console.log("Restaurant :", restaurantName);
+console.log("Customer :", customerName);
+console.log("Food :", foodName);
+console.log("Quantity :", quantity);
+
+// Task-4 Hospital Registration //
+const hospitalName = "City Care Hospital";
+let patientName = prompt("Enter Patient Name");
+let token = 42;
+
+console.log("Hospital :", hospitalName);
+console.log("Patient :", patientName);
+console.log("Token :", token);
+
+
+// Task-5 Movie Ticket Booking //
+const theatreName = "PVR Cinemas";
+let movieName = prompt("Enter Movie Name");
+let customerName = prompt("Enter Customer Name");
+let seat = "C15";
+
+console.log("Theatre :", theatreName);
+console.log("Movie :", movieName);
+console.log("Customer :", customerName);
+console.log("Seat Number :", seat);
+
+
+// Task-6 Mobile Shop//
+const shopName = "Smart Mobile Store";
+let customerName = prompt("Enter Customer Name");
+let mobileModel = prompt("Enter Mobile Model");
+let price = 32000;
+
+console.log("Shop :", shopName);
+console.log("Customer :", customerName);
+console.log("Mobile :", mobileModel);
+console.log("Price :", price);
+
+
+// Task-7 Gym Membership //
+const gymName = "Power Fitness";
+let memberName = prompt("Enter Member Name");
+let membershipfee = 4500;
+
+console.log("Gym :", gymName);
+console.log("Member :", memberName);
+console.log("Membership Fee :", membershipfee);
+
+// Task-8 ATM Welcome Screen//
+const bankName = "HDFC Bank";
+let userName = prompt("Enter Your Name");
+
+console.log("Welcome", userName);
+console.log("Bank :", bankName);
+
+// Task-9 Bus Ticket//
+const travelsName = "VRL Travels";
+let passengerName = prompt("Enter Passenger Name");
+let seat = "D8";
+
+console.log("Travels :", travelsName);
+console.log("Passenger :", passengerName);
+console.log("Seat Number :", seat);
+
+// Task-10 Online Shopping //
+const websiteName = "Flipkart";
+let productName = prompt("Enter Product Name");
+let customerName = prompt("Enter Customer Name");
+let price = 1999;
+
+console.log("Website :", websiteName);
+console.log("Product :", productName);
+console.log("Customer :", customerName);
+console.log("Price :", price);
+
+// Task-11 Print a success message using console.log() //
+console.log("Registration Successful");
+// Task-12 Show an error using console.error() //
+console.error("Payment Failed");
+// Task-13 Show a warning using console.warn()//
+console.warn("Battery Low");
+// Task-14 Print some values and then clear the console using console.clear()//
+console.log("Loading...");
+console.log("Welcome");
+
+console.clear();
+// Task-15 Display: Welcome to Amazon using alert() //
+alert("Welcome to Amazon");
+// Task-16 Ask: Do you want to logout? using confirm().Store the result in a variable and print it//
+let logout = confirm("Do you want to logout?");
+
+console.log(logout);
+
+// Task-17 Ask the user's city using prompt()//
+let city = prompt("Enter Your City");
+
+console.log("Your City :", city);
+// Task-18 //
+var product = "Laptop";
+
+product = "Mobile";
+
+var product = "Tablet";
+
+console.log(product);
+
+// Task-19 //
+
+let salary = 25000;
+salary = 30000;
+console.log(salary);
+
+// Task-20 //
+const company = "Google";
+console.log(company);
